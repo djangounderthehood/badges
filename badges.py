ca9e17e9617e3233f50cf3ba9300d0dd3344f1d7
Ola Sitarska <ola@sitarska.com>
2016-08-25 21:02:35 +0100

    Update script to handle two lines of names, optional preferred names and new template

--- badges.py
+++ badges.py
@@ -4,42 +4,65 @@ from stereo import Layout
 from stereo.fields import TextField
 
 
-class FirstNameField(TextField):
+class NameField(TextField):
     font_name = 'Lato-Heavy'
     font_size = 35
-    fit_text = True
     text_align = TextField.TEXT_ALIGN_CENTER
     color = 'blue'
-    top = 115
+    top = 120
+    left = 55
+    width = 180
+    
+    def get_name(self):
+        ticket_name, preferred_name = self.data.split('||')[0], self.data.split('||')[1]
+        if not preferred_name or preferred_name == '-':
+            return ticket_name.upper()
+        else:
+            return preferred_name.upper()
 
     def render(self):
-        self.data = self.data.upper()
-        super(FirstNameField, self).render()
+        name = self.get_name()
+        
+        first_line = name.split(' ')[0]
+        second_line = name.replace(first_line, '')
+        
+        if not second_line:
+            self.data = first_line
+            super(NameField, self).render()
+        else:
+            self.data = first_line
+            self.top -= int(self.font_size / 2)
+            super(NameField, self).render()
+            
+            self.data = second_line
+            self.top += int(self.font_size / 2) + 10
+            self.font_size -= 10
+            self.font_name = 'Lato-Regular'
+            super(NameField, self).render()
 
 
-class LastNameField(FirstNameField):
-    font_name = 'Lato-Regular'
-    font_size = 23
-    top = 155
-
-
-class RoleField(FirstNameField):
+class RoleField(TextField):
     font_name = 'Lato-Light'
     font_size = 16
     top = 200
+    text_align = TextField.TEXT_ALIGN_CENTER
+    color = 'blue'
+
+    def render(self):
+        self.data = self.data.upper()
+        super(RoleField, self).render()
 
 
 class Badge(Layout):
-    data_file = 'guests.csv'
+    data_file = 'tito.csv'
     template_file = 'template.pdf'
     output_dir = 'output'
     skip_first_row = True
-    width = 338
-    height = 338
+    width = 290
+    height = 290
     fields = [
-        FirstNameField,
-        LastNameField,
-        RoleField
+        RoleField,
+        NameField,
     ]
     fonts = {
         'Lato-Heavy': 'fonts/Lato-Heavy.ttf',
